f9cc48393d9f9f6a7b9bb602dbf3c9a8a1cdaf28
Stephen <brawner@gmail.com>
2015-09-14 14:47:44 -0400

    adding some simple build scripts

--- scripts/upload_to_server.sh
+++ scripts/upload_to_server.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+DEFAULT_SERVER="elzar"
 BURLAP_BRANCH="multi_agent_games"
 MAG_BRANCH="master"
-SERVER=$1
+SERVER=${1:-$DEFAULT_SERVER}
 
 kinit
 cd ~/workspace/burlap
 #maybe check which branch and if it's not correct, then checkout and pull
 git checkout $BURLAP_BRANCH
 git pull
-rsync -avz ./ brawner@$SERVER:~/workspace/burlap
+rsync -avz ./ $SERVER:~/workspace/burlap
 cd ~/workspace/MultiAgentGames
 git checkout $MAG_BRANCH
 git pull
