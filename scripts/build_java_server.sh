9dc9b3b57a6e85d4bafee1e18f748792112ecfbe
Stephen <brawner@gmail.com>
2015-09-15 17:38:41 -0400

    modifying scripts to run closure, and only copy the condensed javascript

--- scripts/build_java_server.sh
+++ scripts/build_java_server.sh
@@ -10,12 +10,13 @@ cp dist/burlap.jar ../MultiAgentGames/lib/
 echo "Updating and building MultiAgentGames"
 cd ../MultiAgentGames
 ant
+java -jar compiler.jar -O SIMPLE --js_output_file=web/all.js 'web/javascript/**.js'
 
 echo "Syncing grid games files"
 mkdir -p ~/grid_games/results
 
 rsync -avz ./resources/ ~/grid_games/
-rsync -avz ./web/ /var/www/multi_grid_games/
+rsync -vz ./web/* /var/www/multi_grid_games/
 sudo chown $USER:www-data -R /var/www/multi_grid_games
 sudo chmod 750 -R /var/www/multi_grid_games
 
